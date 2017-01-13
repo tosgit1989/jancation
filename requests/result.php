cc3e5bdaff3a222a812581771cbdebd4b5e737c7
tosgit1989 <purannaa1989@gmail.com>
2017-01-14 06:03:36 +0900

    対戦結果を表示したりその結果をデータベースに反映させたりするコードを result.phpに追記

--- requests/result.php
+++ requests/result.php
@@ -1,3 +1,38 @@
 <?php
 require_once ('../app.php');
+$request = $dataConnect->getById($RequestId, 'requests');
+
+// hand番号は、グー:1 , チョキ:2 , パー:3
+$handSort = ['0', 'グー', 'チョキ', 'パー'];
+
+// あなたの手($handYou)と相手の手($handAit)
+$handYou = intval($_POST['hand']);
+$handAit = rand(1, 3);
+
+// 判定
+if (($handYou - $handAit + 3) % 3 == 2) {
+    // じゃんけんに勝った場合
+    $judge = "あなたの勝ち";
+    $dataConnect->updateScore('win', ['id' => $UserId]);
+    $dataConnect->updateScore('lose', ['id' => $request['from_user_id']]);
+} elseif (($handYou - $handAit + 3) % 3 == 1) {
+    // じゃんけんに負けた場合
+    $judge = "あなたの負け";
+    $dataConnect->updateScore('lose', ['id' => $UserId]);
+    $dataConnect->updateScore('win', ['id' => $request['from_user_id']]);
+} else {
+    // それ以外の場合
+    $judge = "あいこ";
+}
+
 ?>
+
+<p>あなた: <?php echo $handSort[$_POST['hand']]?>, 相手: <?php echo $handSort[$handAit]?></p>
+<p>判定: <?php echo $judge ?></p>
+<a href="/requests/play.php/<?php echo $RequestId ?>">連戦する</a>
+<a href="" data-toggle="link" onclick="document.LinkToExecPhp.submit();return false;">ゲーム終了</a>
+
+<!--hidden form-->
+<form name="LinkToExecPhp" method="POST" action="/requests/exec.php/<?php echo $RequestId ?>">
+    <input class="form-control" name="exectype" type="hidden" value="exitPlay">
+</form>
