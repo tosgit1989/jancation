788bba06babee8dd7a1262632130eacf7120026c
tosgit1989 <purannaa1989@gmail.com>
2017-01-13 23:45:54 +0900

    sign_in.phpにサインインフォームを作成

--- users/sign_in.php
+++ users/sign_in.php
@@ -1,3 +1,19 @@
 <?php
 require_once ('../app.php');
 ?>
+
+<!--フォーム-->
+<form action="/users/session.php" method="post">
+    <div class="form-group">
+        <!--メールアドレス入力欄-->
+        <p><strong>メールアドレス</strong></p>
+        <input class="form-control" type="text" name="email" placeholder="メールアドレスを入力" value=""><br>
+        <!--パスワード入力欄-->
+        <p><strong>パスワード</strong></p>
+        <input class="form-control" type="password" name="psw" placeholder="パスワードを入力" value=""><br>
+    </div>
+    <div class="form-group">
+        <!--サインインボタン-->
+        <input type="submit" name="sign-in" class="btn btn-primary" value="サインイン">
+    </div>
+</form>
