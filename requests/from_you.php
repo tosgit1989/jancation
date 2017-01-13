f3c362085f3e44498fdd4761c87004ff098c0ef5
tosgit1989 <purannaa1989@gmail.com>
2017-01-14 06:30:22 +0900

    自分からの申請一覧で、各申請の右側に編集ページや削除ページに移動するリンクを作成

--- requests/from_you.php
+++ requests/from_you.php
@@ -6,6 +6,8 @@ require_once ('../app.php');
 foreach ($requests as $request) {
     if ($request['from_user_id'] == $UserId) {
         echo $request['id'];
+        echo sprintf('<a href="/requests/edit.php/%s">編集</a>', $request['id']);
+        echo sprintf('<a href="/requests/delete.php/%s">削除</a>', $request['id']);
         echo '<br>';
     }
 }
