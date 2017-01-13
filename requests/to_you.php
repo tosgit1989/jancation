558f146b55da8298e42a26d9728fd301e6c7c9ed
tosgit1989 <purannaa1989@gmail.com>
2017-01-14 03:26:07 +0900

    自分への申請一覧で、各申請の右側にplay.phpに移動するリンクを作成

--- requests/to_you.php
+++ requests/to_you.php
@@ -6,6 +6,7 @@ require_once ('../app.php');
 foreach ($requests as $request) {
     if ($request['to_user_id'] == $UserId) {
         echo $request['id'];
+        echo sprintf('<a href="/requests/play.php/%s">対戦</a>', $request['id']);
         echo '<br>';
     }
 }
