df34cf0476c18a3fce4eab9d5550ac85a5886e92
tosgit1989 <purannaa1989@gmail.com>
2017-01-14 06:45:38 +0900

    申請新規作成ページに移動するリンクを作成

--- requests/which.php
+++ requests/which.php
@@ -3,5 +3,6 @@ require_once ('../app.php');
 ?>
 
 <h3>いずれか1つを選択</h3>
+<a href="/requests/new.php">じゃんけんを申請する</a>
 <a href="/requests/to_you.php">あなたへの申請一覧</a>
 <a href="/requests/from_you.php">あなたからの申請一覧</a>
