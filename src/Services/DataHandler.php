758ad8fdf764c3f3866c2e0bb0daa95c604f858f
tosgit1989 <purannaa1989@gmail.com>
2017-01-13 22:11:26 +0900

    getPdo()(PDOを取得する関数)を定義

--- src/Services/DataHandler.php
+++ src/Services/DataHandler.php
@@ -1,5 +1,14 @@
 <?php
 namespace Services;
 class DataHandler {
+    // getPdo()
+    protected function getPdo() {
+        $dbConnect = 'mysql:dbname=jancation; host=127.0.0.1; charset=utf8';
+        $username = 'root';
+        $password = '';
+        $driverOptions = [];
+        $pdo = new \PDO($dbConnect, $username, $password, $driverOptions);
+        return $pdo;
+    }
 }
 ?>
