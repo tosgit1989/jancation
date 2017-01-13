294163c75b5ba496f6f918539236e851704f73a5
tosgit1989 <purannaa1989@gmail.com>
2017-01-14 03:02:35 +0900

    ユーザーの基本情報と対戦成績をマイページに表示

--- users/show.php
+++ users/show.php
@@ -1,3 +1,15 @@
 <?php
 require_once ('../app.php');
 ?>
+
+<!--自分の基本情報を表示-->
+<h3 class="text-middle">基本情報</h3>
+<h4>ニックネーム  : <?php echo $user['nickname'] ?></h4>
+<h4>メールアドレス: <?php echo $user['email'] ?></h4>
+
+<!--自分の対戦成績を表示-->
+<h3 class="text-middle"><?php echo $user['nickname'] ?>さんの対戦成績</h3>
+<p>プレイ回数: <?php echo $user['win_count'] + $user['lose_count'] ?></p>
+<p>勝ち回数: <?php echo $user['win_count'] ?></p>
+<p>負け回数: <?php echo $user['lose_count'] ?></p>
+<p>勝率: <?php echo $user['win_rate'] ?>％</p>
