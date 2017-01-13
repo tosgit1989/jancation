624e9d01801eea1f1384a5ba398d8d7b44f38ec0
tosgit1989 <purannaa1989@gmail.com>
2017-01-14 06:16:04 +0900

    exec.phpに、ゲーム終了時の申請削除処理をするコードを追記、またトップページへのリンクも追記

--- requests/exec.php
+++ requests/exec.php
@@ -1,3 +1,10 @@
 <?php
 require_once ('../app.php');
+if ($_POST['exectype'] == 'exitPlay') {
+    // 申請削除時またはゲーム終了時
+    $ExecMessage = '申請を削除しました。';
+    $dataConnect->delete(['id' => $RequestId], 'requests');
+}
 ?>
+
+<a href="/index.php">トップページへ</a>
