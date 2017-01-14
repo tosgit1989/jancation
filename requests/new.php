e68c3cf9f0a4a72faed22e0ceca43121f71ef7bb
tosgit1989 <purannaa1989@gmail.com>
2017-01-14 09:23:52 +0900

    申請入力フォームを作成

--- requests/new.php
+++ requests/new.php
@@ -1,3 +1,13 @@
 <?php
 require_once ('../app.php');
 ?>
+
+<!--フォーム-->
+<form method="POST" action="/requests/exec.php/new">
+    <div class="form-group">
+        <p><strong>申請情報</strong></p>
+        <input required="required" class="form-control" placeholder="対戦相手のIDを入力" name="to_user_id" type="text"><br>
+        <input class="form-control" name="exectype" type="hidden" value="newRequest">
+    </div>
+    <button class="btn btn-primary" type="submit">申請する</button>
+</form>
