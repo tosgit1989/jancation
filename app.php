9575a15f7ba9b406a21e4ef8f9656506899b40e5
tosgit1989 <purannaa1989@gmail.com>
2017-01-14 11:19:29 +0900

    画面左上の「jancation」をクリックするとトップページに移動するようにした。

--- app.php
+++ app.php
@@ -32,7 +32,7 @@ if ($_SERVER['REQUEST_URI'] == '/users/sign_in.php') {
 <nav class="navbar navbar-fixed-top navbar-inverse">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="active navbar-brand" href="#">jancation</a>
+            <a class="active navbar-brand" href="/index.php">jancation</a>
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
