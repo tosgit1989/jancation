767be315f9e67c6c669c31d416aa05a16ed0d462
tosgit1989 <purannaa1989@gmail.com>
2017-01-14 09:42:57 +0900

    申請削除フォームを作成

--- requests/delete.php
+++ requests/delete.php
@@ -1,3 +1,12 @@
 <?php
 require_once ('../app.php');
 ?>
+
+<h3>本当に削除しますか？</h3>
+<form method="POST" action="/requests/exec.php/<?php echo $RequestId ?>">
+    <div class="form-group">
+        <input class="form-control" name="exectype" type="hidden" value="deleteRequest">
+    </div>
+    <button class="btn btn-danger" type="submit">削除</button>
+    <a href="/users/show.php/<?php echo $UserId ?>" class="btn" style="background-color: silver; color: black">いいえ</a>
+</form>
