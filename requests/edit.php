236ed2910fc6047829c598a35ff63f0184bbe0e5
tosgit1989 <purannaa1989@gmail.com>
2017-01-14 09:35:27 +0900

    申請更新フォームを作成

--- requests/edit.php
+++ requests/edit.php
@@ -1,3 +1,13 @@
 <?php
 require_once ('../app.php');
 ?>
+
+<!--フォーム-->
+<form method="POST" action="/requests/exec.php/<?php echo $RequestId ?>">
+    <div class="form-group">
+        <p><strong>申請情報</strong></p>
+        <input required="required" class="form-control" placeholder="対戦相手のIDを入力" name="to_user_id" type="text" value="<?php echo $request['to_user_id'] ?>"><br>
+        <input class="form-control" name="exectype" type="hidden" value="editRequest">
+    </div>
+    <button class="btn btn-primary" type="submit">更新する</button>
+</form>
