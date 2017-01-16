43ab1696a91e076357d209d4ff87b43d3b58942d
tosgit1989 <purannaa1989@gmail.com>
2017-01-16 12:59:41 +0900

    コンテンツをdivタグで4重に囲った

--- users/edit.php
+++ users/edit.php
@@ -6,13 +6,23 @@ require_once ('../app.php');
 <div style="background-color: brown; margin-bottom: 15px">
     <p style="font-family: 'Times New Roman'; font-size: 40px; font-style: italic; color: white">ユーザー情報の編集</p>
 </div>
-<!--フォーム-->
-<form method="POST" action="/users/exec.php/<?php echo $UserId ?>">
-    <div class="form-group">
-        <p><strong>メールアドレス</strong></p>
-        <input required="required" class="form-control" placeholder="メールアドレスを入力" name="email" type="text" value="<?php echo $user['email'] ?>"><br>
-        <p><strong>ニックネーム</strong></p>
-        <input required="required" class="form-control" placeholder="ニックネームを入力" name="nickname" type="text" value="<?php echo $user['nickname'] ?>">
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="bs-docs-section">
+
+                <!--フォーム-->
+                <form method="POST" action="/users/exec.php/<?php echo $UserId ?>">
+                    <div class="form-group">
+                        <p><strong>メールアドレス</strong></p>
+                        <input required="required" class="form-control" placeholder="メールアドレスを入力" name="email" type="text" value="<?php echo $user['email'] ?>"><br>
+                        <p><strong>ニックネーム</strong></p>
+                        <input required="required" class="form-control" placeholder="ニックネームを入力" name="nickname" type="text" value="<?php echo $user['nickname'] ?>">
+                    </div>
+                    <button type="submit" class="btn btn-primary">更新する</button>
+                </form>
+
+            </div>
+        </div>
     </div>
-    <button type="submit" class="btn btn-primary">更新する</button>
-</form>
+</div>
