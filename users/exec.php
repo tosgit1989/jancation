39c2aa25cd368f3fa492e4f79cf0fcf743960cdf
tosgit1989 <purannaa1989@gmail.com>
2017-01-16 12:14:09 +0900

    exec.phpに更新処理のコードを追記→ユーザー情報をを更新できるようにした。

--- users/exec.php
+++ users/exec.php
@@ -1,3 +1,9 @@
 <?php
 require_once ('../app.php');
+
+$userUpd['nickname'] = $_POST['nickname'];
+$userUpd['email'] = $_POST['email'];
+$userUpd['updated_at'] = $now = date('Y/m/d H:i:s');
+$dataConnect->update($userUpd, ['id' => $UserId], 'users');
+header('Location: /index.php');
 ?>
