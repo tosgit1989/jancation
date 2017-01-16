5d0c13a5a982f6eb4031c6ff15f2bc7b323fffbb
tosgit1989 <purannaa1989@gmail.com>
2017-01-16 12:13:31 +0900

    ユーザー情報を編集するフォームを作成

--- users/edit.php
+++ users/edit.php
@@ -1,3 +1,14 @@
 <?php
 require_once ('../app.php');
 ?>
+
+<!--フォーム-->
+<form method="POST" action="/users/exec.php/<?php echo $UserId ?>">
+    <div class="form-group">
+        <p><strong>メールアドレス</strong></p>
+        <input required="required" class="form-control" placeholder="メールアドレスを入力" name="email" type="text" value="<?php echo $user['email'] ?>"><br>
+        <p><strong>ニックネーム</strong></p>
+        <input required="required" class="form-control" placeholder="ニックネームを入力" name="nickname" type="text" value="<?php echo $user['nickname'] ?>">
+    </div>
+    <button type="submit" class="btn btn-primary">更新する</button>
+</form>
