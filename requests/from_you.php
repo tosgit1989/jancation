7e9b03a30398d338eece78548ea08b1fbdbff68a
tosgit1989 <purannaa1989@gmail.com>
2017-02-04 12:38:34 +0900

    %%sを%sに修正

--- requests/from_you.php
+++ requests/from_you.php
@@ -15,7 +15,7 @@ $FromUser = $dataConnect->getById($UserId, 'users');
                 foreach ($requests as $request) {
                     if ($request['from_user_id'] == $UserId) {
                         $ToUser = $dataConnect->getById($request['to_user_id'], 'users');
-                        $HeadingHtml = sprintf('%%s→%s', $FromUser['nickname'], $ToUser['nickname']);
+                        $HeadingHtml = sprintf('%s→%s', $FromUser['nickname'], $ToUser['nickname']);
                         $BodyHtml = sprintf('<a href="/requests/edit.php/%s" class="btn btn-primary" role="button">編集</a>', $request['id'])
                                     . sprintf('<a href="/requests/delete.php/%s" class="btn btn-danger" role="button">削除</a>', $request['id']);
                         $FooterHtml = sprintf('申請日時: %s 更新日時: %s', $request['created_at'], $request['updated_at']);
