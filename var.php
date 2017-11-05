e302fd0aaea6989a68f7b74138a906c09709d8b9
tosgit1989 <purannaa1989@gmail.com>
2017-11-05 10:32:59 +0900

    zennsakujo

--- var.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once ('src/Services/DataHandler.php');
-require_once ('src/Services/Methods.php');
-$dataConnect = new \Services\DataHandler();
-$methods = new \Services\Methods();
-$requests = $dataConnect->getAll('requests');
-$requestId = $methods->getRequestId($_SERVER['REQUEST_URI']);
-?>
