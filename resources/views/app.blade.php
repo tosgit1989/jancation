ba737e9660b7b21bfb1b3081809802c39ecb6cf3
tosgit1989 <purannaa1989@gmail.com>
2017-11-05 17:44:52 +0900

    Laravel系のファイルに差し替え

---
+++ resources/views/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+	<head>
+		<meta charset="utf-8">
+		<title>jancation</title>
+		<link rel="stylesheet" href="/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/css/application.css">
+	</head>
+	<body style="background-color:white">
+
+		<!--ヘッダー-->
+		<nav class="navbar navbar-fixed-top navbar-inverse">
+			<div class="container-fluid">
+				<div class="navbar-header" style="max-height: 50px">
+					<a class="active navbar-brand">jancation</a>
+				</div>
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+					</ul>
+				</div>
+			</div>
+		</nav>
+
+		<!--フッター-->
+		<footer class="bs-docs-footer navbar-fixed-bottom" style="background-color: #000000; height: 30px">
+			<div class="container">
+				<p class="text-muted"></p>
+			</div>
+		</footer>
+
+	</body>
+</html>
