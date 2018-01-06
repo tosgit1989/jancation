e1c22cfc1a2160c6fc45ca6ce30f8bb973dede53
tosgit1989 <purannaa1989@gmail.com>
2018-01-07 07:07:52 +0900

    database.php変更

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
 		'mysql' => [
 			'driver' => 'mysql',
-			'host' => env('DB_HOST', '127.0.0.1'),
+			'host' => env('DB_HOST', 'localhost'),
 			'port' => env('DB_PORT', '3306'),
 			'database' => env('DB_DATABASE', 'forge'),
 			'username' => env('DB_USERNAME', 'forge'),
@@ -56,7 +56,7 @@ return [
 
 		'pgsql' => [
 			'driver' => 'pgsql',
-			'host' => env('DB_HOST', '127.0.0.1'),
+			'host' => env('DB_HOST', 'localhost'),
 			'port' => env('DB_PORT', '5432'),
 			'database' => env('DB_DATABASE', 'forge'),
 			'username' => env('DB_USERNAME', 'forge'),
