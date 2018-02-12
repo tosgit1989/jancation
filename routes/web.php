cbba7a30284863e1bfd8872f2961f0e5334f2fed
tosgit1989 <purannaa1989@gmail.com>
2018-02-12 10:17:29 +0900

    ルーティングファイル更新

--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,92 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [
+	'uses' => 'TopPageController@index',
+	'as' => 'top'
+]);
 
-Route::get('/', function () {
-	return view('index');
-});
+Route::get('/showsignin', [
+    'uses' => 'AuthController@showSignIn',
+    'as' => 'top'
+]);
 
-Route::get('/menu', function () {
-	return view('menu');
-});
+Route::get('/dosignin', [
+    'uses' => 'AuthController@doSignIn',
+    'as' => 'top'
+]);
 
-Route::get('/mypage', function () {
-	return view('mypage');
-});
+Route::get('/dosignout', [
+    'uses' => 'AuthController@doSignOut',
+    'as' => 'top'
+]);
 
-Route::get('/play1', function () {
-	return view('play1');
-});
+Route::get('/showsignup', [
+    'uses' => 'AuthController@showSignUp',
+    'as' => 'top'
+]);
 
-Route::get('/play2', function () {
-	return view('play2');
-});
+Route::get('/dosignup', [
+    'uses' => 'AuthController@doSignUp',
+    'as' => 'top'
+]);
+
+Route::get('/menu', [
+	'uses' => 'MenuController@index',
+	'as' => 'top'
+]);
+
+Route::get('/mypage', [
+	'uses' => 'MyPageController@index',
+	'as' => 'top'
+]);
+
+Route::get('/myplayrequests', [
+	'uses' => 'MyPlayRequestsController@index',
+	'as' => 'top'
+]);
+
+Route::get('/newplayrequest', [
+	'uses' => 'ExecPlayRequestController@getNew',
+	'as' => 'create'
+]);
+
+Route::post('/newplayrequest', [
+	'uses' => 'ExecPlayRequestController@postNew',
+	'as' => 'post_create'
+]);
+
+Route::get('/editplayrequest/{id}', [
+	'uses' => 'ExecPlayRequestController@getEdit',
+	'as' => 'edit'
+]);
+
+Route::post('/editplayrequest/{id}', [
+	'uses' => 'ExecPlayRequestController@postEdit',
+	'as' => 'post_edit'
+]);
+
+Route::get('/deleteplayrequest/{id}', [
+	'uses' => 'ExecPlayRequestController@getDelete',
+	'as' => 'destroy'
+]);
+
+Route::post('/deleteplayrequest/{id}', [
+	'uses' => 'ExecPlayRequestController@postDelete',
+	'as' => 'post_destroy'
+]);
+
+Route::get('/playselect', [
+	'uses' => 'PlayController@select',
+	'as' => 'top'
+]);
+
+Route::get('/playhand/{id}', [
+    'uses' => 'PlayController@hand',
+    'as' => 'top'
+]);
+
+Route::get('/playresult/{id1}/{id2}', [
+    'uses' => 'PlayController@result',
+    'as' => 'top'
+]);
