168e5e1a61c64dcf63c70f33f1946f0e1664b638
tosgit1989 <purannaa1989@gmail.com>
2018-04-08 17:36:13 +0900

    コントローラーファイルでもAuth::user()を利用できるようにした

--- app/Http/Controllers/ExecPlayRequestController.php
+++ app/Http/Controllers/ExecPlayRequestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Models\PlayRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\FuncController;
 use App\Http\Requests;
 use Carbon\Carbon;
