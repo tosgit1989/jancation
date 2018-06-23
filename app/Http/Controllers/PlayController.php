64dc25e04c7203a2e459fa105ee3a1fdf6ade73c
tosgit1989 <purannaa1989@gmail.com>
2018-06-23 16:35:19 +0900

    PlayControllerについてもPlayRequestsControllerと同じように修正

--- app/Http/Controllers/PlayController.php
+++ app/Http/Controllers/PlayController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use DB;
 use App\Http\Models\PlayLog;
 use App\Http\Models\PlayRequest;
+use App\Http\Models\PlayScore;
 use Illuminate\Http\Request;
 use App\Http\Controllers\FuncController;
 use Carbon\Carbon;
@@ -69,16 +70,16 @@ class PlayController extends Controller
 		}
 		$curDateTime = new Carbon();
 		$AiteHandNum = rand(1, 3);
-		$YourHand = FuncController::Hand($YourHandNum);
-		$AiteHand = FuncController::Hand($AiteHandNum);
-		$Judge = FuncController::Judge($YourHandNum, $AiteHandNum);
+		$YourHand = $this->getHand($YourHandNum);
+		$AiteHand = $this->getHand($AiteHandNum);
+		$Judge = $this->Judge($YourHandNum, $AiteHandNum);
 		//  勝負がついた場合
 		if($Judge !== 'あいこ')
 		{
 			$YourWin = ($Judge == 'あなたの勝ち') ? 1 : 0;
 			//  プレイスコア更新
-			$curPlayScore1 = FuncController::findByUserId($curPlayRequest->to_user_id);
-			$curPlayScore2 = FuncController::findByUserId($curPlayRequest->from_user_id);
+			$curPlayScore1 = $this->findByUserId($curPlayRequest->to_user_id);
+			$curPlayScore2 = $this->findByUserId($curPlayRequest->from_user_id);
 			$WinCountBef1 = $curPlayScore1->win_count;
 			$WinCountBef2 = $curPlayScore2->win_count;
 			$LoseCountBef1 = $curPlayScore1->lose_count;
@@ -98,8 +99,9 @@ class PlayController extends Controller
 			//  プレイリクエスト更新
 			if(!isset($curPlayRequest->expired_at))
 			{
-				$curPlayRequest->expired_at = $curDateTime;
-				$curPlayRequest->save();
+				$EditPlayRequest = PlayRequest::find($curPlayRequest->id);
+				$EditPlayRequest->expired_at = $curDateTime;
+				$EditPlayRequest->save();
 			}
 		}
 
@@ -110,7 +112,40 @@ class PlayController extends Controller
 			'Judge' => $Judge
 		]);
 	}
+	
+	protected function findByUserId($UserId)
+	{
+		$PlayScores = PlayScore::all()->where('user_id', $UserId);
+		if(count($PlayScores) == 0)
+		{
+			//  まだ作成していない場合は作成
+			$NewPlayScore = new PlayScore();
+			$NewPlayScore->user_id = $UserId;
+			$NewPlayScore->win_count = 0;
+			$NewPlayScore->lose_count = 0;
+			$NewPlayScore->save();
+			$PlayScore = $NewPlayScore;
+		}
+		else
+		{
+			$PlayScore = $PlayScores->first();
+		}
+		return $PlayScore;
+	}
 
+	protected function getHand($HandId)
+	{
+		$HandList = [ 1 => 'グー', 2 => 'チョキ', 3 => 'パー' ];
+		return $HandList[$HandId];
+	}
+
+	protected function Judge($YourHandId, $AiteHandId)
+	{
+		$JudgeId = ($YourHandId > $AiteHandId) ? $YourHandId - $AiteHandId : $YourHandId - $AiteHandId + 3;
+		$JudgeList = [ 1 => 'あなたの負け', 2 => 'あなたの勝ち', 3 => 'あいこ' ];
+		return $JudgeList[$JudgeId];
+	}
+	
 	protected function RedirectToError($BackTo)
 	{
 		return view('error')->with([
